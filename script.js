18f6170e32ef72e1028aaea2dafd4fd529abf0f7
kiyoto0722 <0wk38d0w2gf5z5y@gmail.com>
2026-03-12 18:23:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let currentCity = null;
 let questionCount = 0;
 let correctCount = 0;
 let maxQuestion = 5;
+let gameOver=false;
 
 fetch("tokyo_cities.geojson")
 .then(res=>res.json())
@@ -66,8 +67,12 @@ fetch("tokyo_cities.geojson")
                 questionCount++;
 
                 if(questionCount>=maxQuestion){
-
-                    setTimeout(showResult,500);
+                    gameOver=true;
+                    document.getElementById("finalScore").innerHTML=
+                        "スコア "+score+" / 5";
+                    document.getElementById("gameoverPopup").style.display="flex";
+                    return;
+                }
 
                 }else{
 
@@ -103,19 +108,19 @@ function nextQuestion(){
     "第"+(questionCount+1)+"問 : "+currentCity;
 }
 
-function showResult(){
+//function showResult(){
 
-    let rank="";
+//    let rank="";
 
-    if(correctCount==5) rank="S";
-    else if(correctCount>=4) rank="A";
-    else if(correctCount>=3) rank="B";
-    else if(correctCount>=2) rank="C";
-    else rank="D";
+//    if(correctCount==5) rank="S";
+//    else if(correctCount>=4) rank="A";
+//    else if(correctCount>=3) rank="B";
+//    else if(correctCount>=2) rank="C";
+//    else rank="D";
 
-    document.getElementById("question").innerText=
-    "終了！ "+correctCount+" / 5 正解 ランク "+rank;
-}
+//    document.getElementById("question").innerText=
+//    "終了！ "+correctCount+" / 5 正解 ランク "+rank;
+//}
 
 function showJudge(correct){
 
@@ -135,4 +140,6 @@ function showJudge(correct){
 }
 
 document.getElementById("startBtn").onclick=startGame;
-document.getElementById("startBtn").style.display="none";
+document.getElementById("restartBtn").onclick=function(){
+    location.reload();
+}
